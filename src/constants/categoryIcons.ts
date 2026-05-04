a9b85ec036c4f8b9ff83d5ce6057a33c14b72afa
Brad <brad.habenicht@gmail.com>
2026-05-04 15:12:53 -0400

    chore: add category icon map

---
+++ src/constants/categoryIcons.ts
@@ -0,0 +1,31 @@
+interface categoryIconsInterface {
+  [key: string]: string;
+}
+
+export const categoryIcons: categoryIconsInterface = {
+  'beauty': 'spa',
+  'fragrances': 'waves',
+  'furniture': 'bed',
+  'groceries': 'fastfood',
+  'home-decoration': 'deck',
+  'kitchen-accessories': 'kitchen',
+  'laptops': 'laptop',
+  'mens-shirts': 'man',
+  'mens-shoes': 'snowshoeing',
+  'mens-watches': 'watch',
+  'mobile-accessories': 'smart_toy',
+  'motorcycle': 'directions_bike',
+  'skin-care': 'wash',
+  'smartphones': 'smartphone',
+  'sports-accessories': 'sports_football',
+  'sunglasses': 'wb_sunny',
+  'tablets': 'tablet_mac',
+  'tops': 'kitesurfing',
+  'vehicle': 'fire_truck',
+  'womens-bags': 'shopping_bag',
+  'womens-dresses': 'woman',
+  'womens-jewellery': 'diamond',
+  'womens-shoes': 'sports_gymnastics',
+  'womens-watches': 'timer',
+  'default': 'mood'
+}
