d639fa2bed3839bb3c02290a70cb9f5517651338
Brad <brad.habenicht@gmail.com>
2026-05-04 15:12:30 -0400

    chore: update header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,12 +20,14 @@ function Header() {
               <span className='material-icons-sharp search-icon'>search</span>
               <span className='search-text'>Search for products</span>
             </div>
-            <Link className='cart-link' to='cart'>
-              <span className="material-icons-outlined header-icon">shopping_cart</span>
-            </Link>
-            <Link className='account-link' to='account'>
-              <span className="material-icons-outlined header-icon">account_circle</span>
-            </Link>
+            <div className='header-nav'>
+              <Link className='cart-link' to='cart'>
+                <span className="material-icons-outlined header-icon">shopping_cart</span>
+              </Link>
+              <Link className='account-link' to='account'>
+                <span className="material-icons-outlined header-icon">account_circle</span>
+              </Link>
+            </div>
           </div>
         </div>
         <nav>
