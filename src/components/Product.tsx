2274a92ac33f0dc8e825c1bad25478a278730fb5
Brad <brad.habenicht@gmail.com>
2026-05-13 13:52:32 -0400

    feat: add basic product data to pdp

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,11 +1,11 @@
-import '../styles/Product.css';
+import '../styles/ProductComponent.css';
 import { Link } from "react-router";
 import type { Product } from "../types/productTypes";
 import defaultImage from '../assets/5191452-200.png';
 
 export const ProductComponent = (product: Product) => {
   return (
-    <Link className='product-container' to='Product' key={product.title}>
+    <Link className='product-container' to={`product/${product.id}`} key={product.title}>
       <img src={product?.images?.[0] || defaultImage} className='product-image' />
       <div className='product-title'>
         {product.title}
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,7 +1,52 @@
+import '../styles/ProductPage.css';
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router"
+import datasource from "../datasource/datasource";
+import defaultImage from '../assets/5191452-200.png';
+
 export function Product() {
+  const params = useParams();
+  const queryKey = params.id ? 'product' + params.id : 'empty-product-key';
+  const productQuery = useQuery({
+    queryKey: [queryKey, params.id],
+    queryFn: () => datasource.fetchProduct(params.id || '')
+  });
+
+  const renderProduct = () => {
+    const { data } = productQuery;
+
+    if (!data) {
+      return (
+        <div>Sorry, there was a problem loading this item</div>
+      )
+    }
+
+    return (
+      <div className='product-data-container'>
+        <h1 className='product-page-title'>{data.title}</h1>
+        <div className='product-page-info'>
+          <div className='product-page-left'>
+            <img src={data.images?.[0] || defaultImage} className='product-page-image' />
+            <div className='product-page-price'>{data.price && `Price: $${data.price}`}</div>
+          </div>
+          <div className='product-page-data-box'>
+            <div className='product-page-description'>{data.description && data.description}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  const renderLoading = () => {
+    return (
+      <div>Loading...</div>
+    );
+  }
+
   return (
     <div className='product-page-container'>
-      Welcome to the product page
+      {productQuery.isPending && renderLoading()}
+      {productQuery.data && renderProduct()}
     </div>
   )
 }
